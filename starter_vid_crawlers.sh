653e1dba74373eb810e71a3823fe78f9ef847c0f
codeforfun200 <96596881+codeforfun200@users.noreply.github.com>
2022-01-04 21:09:13 +0100

    Add files via upload

--- starter_vid_crawlers.sh
+++ starter_vid_crawlers.sh
@@ -46,12 +46,42 @@ return 1
 }
 
 
+PARAMS=""
+html_output=1
+while (( "$#" )); do
+  case "$1" in
+  #  -a|--my-boolean-flag)
+  #    MY_FLAG=0
+  #    shift
+  #    ;;
+    -o)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        HTML_FILE=$2
+        html_output=0
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS \"$1\""
+      shift
+      ;;
+  esac
+done
+ #set positional arguments in their proper place
+eval set -- "$PARAMS"
 
 
 
 if [ $# -ne 2 ]
   then 
-    echo "Usage $0 \"title\" \"main actor/actress\""
+    echo "Usage $0 [-o filename] \"title\" \"main actor/actress\""
     exit 1
 fi
 
@@ -243,8 +273,8 @@ while read line
           then
       
            part=$(echo "$part" | tr '[:upper:]' '[:lower:]')
-           title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-           titul_in_str "$part" "$title"
+           #title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+           titul_in_str "$part" "$title_ascii"
            if [ $? -eq 0 ]
              then
                #echo "$line"
@@ -275,8 +305,8 @@ while read line
           then
       
             part=$(echo "$part" | tr '[:upper:]' '[:lower:]')
-            title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-            titul_in_str "$part" "$title"
+            #title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+            titul_in_str "$part" "$title_ascii"
             if [ $? -eq 0 ]
               then
             #echo "$line"
@@ -354,4 +384,185 @@ if [ -e multiple_links.txt ]
     cat multiple_links.txt
   else
     echo "No urls found."  
-fi    
+fi   
+
+if [ $html_output = 0 ]
+  then
+    echo "<html>" > $HTML_FILE
+    echo "<head>" >> $HTML_FILE
+    echo "<style> li a{color:red} h2 {margin-top:40px} </style>" >> $HTML_FILE
+    echo "<title>Results of $1 (starring $2) search:</title>" >> $HTML_FILE
+    echo "</head>" >> $HTML_FILE
+    echo "<body>" >> $HTML_FILE
+    echo "<h1>Results of $1 (starring $2) search </h1>" >> $HTML_FILE
+    echo "<h2>Sites with direct download links:</h2>" >> $HTML_FILE
+    echo "<span style=\"color:blue\">blue color</span> - sites where download links were found<br>" >> $HTML_FILE
+    echo "<span style=\"color:red\">red color</span> - found download links<br>" >> $HTML_FILE
+    echo "<ol>" >> $HTML_FILE
+    if [ -e download_lnks.txt ]
+    then
+    while read line
+    do
+    if [[ $line = "For url"* ]]
+      then 
+               
+        echo "</ol>" >> $HTML_FILE
+        echo "<a href=\"${line:8:$((${#line}-8))}\">${line:8:$((${#line}-8))}</a>" >> $HTML_FILE
+        echo "<ol>" >> $HTML_FILE
+      else
+        
+        #zpracing filenamu
+        title=$(echo "$1" | iconv -f utf-8 -t ascii//TRANSLIT  | tr '[:upper:]' '[:lower:]')
+        titul_nalezen=1
+        while read -d '/' part
+          do
+            : 
+        done < <(echo "$line" | cut -d ' ' -f1)
+        #echo "po cyklu je part $part"
+        nalezen=1 
+        for str_in_cycle in "${koncovky[@]}"
+          do
+            if [[ $part = *$str_in_cycle ]]
+              then
+                if [[ $part = *"-"* ]] || [[ $part = *"_"* ]] || [[ $part = *"%20"* ]] || [[ ! $title = *" "* ]] #nove 4.1.
+                  then
+                    nalezen=0
+                    break
+                fi    
+            fi  
+        done  
+        if [ $nalezen -eq 0 ]
+          then
+      
+           part=$(echo "$part" | tr '[:upper:]' '[:lower:]')
+           #title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+           titul_in_str "$part" "$title"
+           if [ $? -eq 0 ]
+             then
+               #echo "$line"
+               titul_nalezen=0
+           fi   
+      #else
+        #echo $line
+        fi 
+    #konec zpracing filenamu
+    
+    #zacatek zpracing hodnoty
+    #while read part
+    #  do
+    #    : 
+    #done < <(echo "$line" | cut -d '(' -f2)  
+        part=$(echo "$line" | cut -d '(' -f2)  
+    
+        nalezen2=1 
+        for str_in_cycle in "${koncovky[@]}"
+          do
+            if [[ $part = *$str_in_cycle* ]] 
+              then
+                nalezen2=0
+                break
+            fi  
+        done  
+        if [ $nalezen2 -eq 0 ]
+          then
+      
+            part=$(echo "$part" | tr '[:upper:]' '[:lower:]')
+            #title=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+            titul_in_str "$part" "$title"
+            if [ $? -eq 0 ]
+              then
+            #echo "$line"
+                titul_nalezen=0
+            fi   
+      #else
+        #echo $line
+        fi 
+    
+        if [ $titul_nalezen -eq 0 ] || ([ $nalezen -eq 1 ] && [ $nalezen2 -eq 1 ]) #kdyz titul nalezen nebo koncovka nenalezena
+          then
+            odkaz=$(echo "$line" | cut -d "(" -f1)
+            if [[ $part = ")" ]] || [[ $part = " )" ]] #part ted obsahuje hodnotovou cast odkazu v zavorkach
+              then
+                part="link has no value"
+            fi  
+            if [[ ! $odkaz = "" ]]
+               then
+                 echo "<li><a href=\"$odkaz\">${part%)}</a></li>" >> $HTML_FILE
+            fi     
+        fi  
+        
+        
+        
+        
+    fi  
+    done < <(cat download_lnks.txt | awk '!seen[$0]++')
+    else #if download_lnks does not exist
+      echo "No sites with direct download links found." >> $HTML_FILE
+    fi
+    
+    echo "</ol>" >> $HTML_FILE
+    echo "<h2>Sites with embedded player (online streaming):</h2>" >> $HTML_FILE
+    if [ -e players.txt ]
+      then
+        
+        while read line
+          do
+            echo "<a href=\"$line\">$line</a><br>" >> $HTML_FILE
+        done <players.txt
+      else 
+        echo "<p>No sites of that type found.</p>" >> $HTML_FILE
+    fi    
+    
+    echo "<h2>Youtube alternative sites:</h2>" >> $HTML_FILE
+    if [ -e youtube_alter.txt ]
+      then
+        
+        while read line
+          do
+            echo "<a href=\"$line\">$line</a><br>" >> $HTML_FILE
+        done <youtube_alter.txt
+      else 
+        echo "<p>No sites of that type found.</p>" >> $HTML_FILE
+    fi    
+    
+    echo "<h2>Paysites:</h2>" >> $HTML_FILE
+    if [ -e paysites.txt ]
+      then
+        
+        while read line
+          do
+            echo "<a href=\"$line\">$line</a><br>" >> $HTML_FILE
+        done <paysites.txt
+      else 
+        echo "<p>No sites of that type found.</p>" >> $HTML_FILE
+    fi    
+    
+    
+    echo "<h2>Torrent sites:</h2>" >> $HTML_FILE
+    if [ -e torrents.txt ]
+      then
+        
+        while read line
+          do
+            echo "<a href=\"$line\">$line</a><br>" >> $HTML_FILE
+        done <torrents.txt
+      else 
+        echo "<p>No sites of that type found.</p>" >> $HTML_FILE
+    fi    
+    
+    echo "<h2>Another sites with potential download links (unsure):</h2>" >> $HTML_FILE
+    if [ -e multiple_links.txt ]
+      then
+        
+        while read line
+          do
+            echo "<a href=\"$line\">$line</a><br>" >> $HTML_FILE
+        done <multiple_links.txt
+      else 
+        echo "<p>No sites of that type found.</p>" >> $HTML_FILE
+    fi    
+    
+    echo "</body>" >> $HTML_FILE
+    echo "</html>" >> $HTML_FILE
+fi  
+ 
